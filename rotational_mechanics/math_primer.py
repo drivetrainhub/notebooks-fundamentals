dbc823cbce6c86906db573e192bf8b7bc502eff5
Chad\YOGA <glinskyc@gmail.com>
2019-05-17 12:16:20 -0600

    Adds skeleton of Rotational Mechanics / Math Primer.

---
+++ rotational_mechanics/math_primer.py
@@ -0,0 +1,5 @@
+# Copyright 2019 Drivetrain Hub LLC
+# For non-commercial use only.  For commercial products and services, visit https://www.drivetrainhub.com.
+
+"""Notebook module for rotational mechanics math primer."""
+
